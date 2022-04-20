e51a87e0c5702f408703fd5518b28af4142d5c09
Topias Heinonen <topias.heinonen@gofore.com>
2022-04-20 18:06:21 +0300

    Tehtävä 4.2

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-require("dotenv").config();
+const app = require("./app");
 const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
 const config = require("./utils/config");
-const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
-const middleware = require("./utils/middleware");
 
-mongoose.connect(config.MONGODB_URI);
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-app.use("/api/blogs", blogsRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
